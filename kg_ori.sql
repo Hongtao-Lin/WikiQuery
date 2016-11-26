00fd7273868347be8dfbccef3c9a3e68ce12b663
Hunter-Lin <Linkin_HearT@sjtu.edu.cn>
2016-11-26 21:50:55 +0800

    data import almost fin

--- kg_ori.sql
+++ kg_ori.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS Entity (
 	ns				smallint 		not null,
 	lastrevid		int 			not null,
 	modified		char(20) 		not null,
-	type			text 		not null
+	type			varchar(8) 		not null
 );
 
 -- Inheritance: entity/property
@@ -17,67 +17,60 @@ CREATE TABLE IF NOT EXISTS Item (
 
 CREATE TABLE IF NOT EXISTS Property (
 	eid				varchar(20),
-	datatype		text 	not null,
+	datatype		varchar(20) 	not null,
 	FOREIGN KEY (eid) REFERENCES entity (eid)
 );
 
 -- Sitelink for item
 CREATE TABLE IF NOT EXISTS Sitelink (
 	eid				varchar(20),
-	site 			varchar(128) 	not null,
-	title 			varchar(128) 	not null,
+	site 			varchar(20) 	not null,
+	title 			varchar(64) 	not null,
 	CONSTRAINT pk_Sitelink PRIMARY KEY (site, title),
 	FOREIGN KEY (eid) REFERENCES item (eid) -- Any problems here?
 );
 
 CREATE TABLE IF NOT EXISTS Badge (
-	site 			varchar(128) 	not null,
-	title 			varchar(128) 	not null,
-	bid				varchar(20) 	not null
-	-- CONSTRAINT fk_b_s FOREIGN KEY (site, title) REFERENCES Sitelink (site, title)
+	site 			varchar(20) 	not null,
+	title 			varchar(64) 	not null,
+	bid				varchar(20) 	not null,
+	CONSTRAINT fk_b_s FOREIGN KEY (site, title) REFERENCES Sitelink (site, title)
 );
 
 -- Fingerprint
 
 CREATE TABLE IF NOT EXISTS Label (
 	eid				varchar(20),
-	language		text,
-	value 			text,
+	language		varchar(8),
+	value 			varchar(128),
 	FOREIGN KEY (eid) REFERENCES Entity (eid)
 );
 
 CREATE TABLE IF NOT EXISTS Description (
 	eid				varchar(20),
-	language		text,
-	value 			text,
+	language		varchar(8),
+	value 			varchar(128),
 	FOREIGN KEY (eid) REFERENCES Entity (eid)
 );
 
 CREATE TABLE IF NOT EXISTS Alias (
 	eid				varchar(20),
-	language		text,
-	value 			text,
+	language		varchar(8),
+	value 			varchar(128),
 	FOREIGN KEY (eid) REFERENCES Entity (eid)
 );
 
 -- Datavalue
 
 CREATE TABLE IF NOT EXISTS Datavalue (
-	did				int 	AUTO_INCREMENT 	PRIMARY KEY,
-	valuetype		text 	not null
+	did				int 			PRIMARY KEY,
+	valuetype		varchar(32) 	not null
 );
 
 
 CREATE TABLE IF NOT EXISTS String ( -- Shall we reserve this type??
 	did				int,
-	value 			text,
-	FOREIGN KEY (did) REFERENCES Datavalue (did)
-);
-
-CREATE TABLE IF NOT EXISTS Monolingualtext ( -- Shall we reserve this type??
-	did				int,
-	language		text,
-	value 			text,
+	value 			varchar(128),
 	FOREIGN KEY (did) REFERENCES Datavalue (did)
 );
 
@@ -93,7 +86,7 @@ CREATE TABLE IF NOT EXISTS Globecoordinate (
 	latitude		real 			not null,
 	longitude		real 			not null,
 	prec			int 			not null,
-	globe			text 	not null,
+	globe			varchar(64) 	not null,
 	altitude		real,
 	FOREIGN KEY (did) REFERENCES Datavalue (did)
 
@@ -102,35 +95,35 @@ CREATE TABLE IF NOT EXISTS Globecoordinate (
 CREATE TABLE IF NOT EXISTS Quantity (
 	did 			int,
 	amount			real 			not null,
-	upperBound		text 	not null,
-	lowerBound		text 	not null,
-	unit			text 	not null,
+	upperBound		varchar(32) 	not null,
+	lowerBound		varchar(32) 	not null,
+	unit			varchar(64) 	not null,
 	FOREIGN KEY (did) REFERENCES Datavalue (did)
 );
 
 CREATE TABLE IF NOT EXISTS Time (
 	did 			int,
-	value			text 	not null,
+	value			varchar(32) 	not null,
 	timezone		smallint 		not null,
 	bef				int 			not null,
 	aft				int 			not null,
 	prec			int 			not null,
-	calendarmodel	text 	not null,
+	calendarmodel	varchar(64) 	not null,
 	FOREIGN KEY (did) REFERENCES Datavalue (did)
 );
 
 
 -- Claim
 CREATE TABLE IF NOT EXISTS Claim (
-	cid				varchar(128) 		PRIMARY KEY, 
-	type			text 		not null, -- statement or not
-	snaktype		text		not null,
+	cid				char(40) 		PRIMARY KEY, 
+	type			varchar(9) 		not null, -- statement or not
+	snaktype		varchar(9)		not null,
 	-- For snak
 	property		varchar(20)		not null,
 	rank			smallint, -- Use 0,1,2 -> preferred, normal, deprecated
-	datatype		text,
-	valuetype		text,
-	value 			text, -- a representative value for faster access
+	datatype		varchar(20),
+	valuetype		varchar(20),
+	value 			varchar(32), -- a representative value for faster access
 	did 			int,
 	FOREIGN KEY (property) REFERENCES Property (eid),
 	FOREIGN KEY (did) REFERENCES Datavalue (did)
@@ -139,36 +132,35 @@ CREATE TABLE IF NOT EXISTS Claim (
 -- Note that Qualifier do not have `type` field
 CREATE TABLE IF NOT EXISTS Qualifier ( -- Shall we join the claim/qualifiers?
 	qid 			char(40) 		PRIMARY KEY, -- For hash!
-	cid				varchar(128),
-	snaktype		text		not null,
+	cid				char(40),
+	snaktype		varchar(9)		not null,
 	property		varchar(20)		not null,
 	rank			smallint, -- Calculated from q_order
-	datatype		text,
-	valuetype		text,
-	value 			text, -- a representative value for faster access
+	datatype		varchar(20),
+	valuetype		varchar(20),
+	value 			varchar(32), -- a representative value for faster access
 	did 			int 			not null,
 	FOREIGN KEY (property) REFERENCES Property (eid),
 	FOREIGN KEY (cid) REFERENCES Claim (cid),
 	FOREIGN KEY (did) REFERENCES Datavalue (did)
 );
 
-CREATE TABLE IF NOT EXISTS ReferneceItem (
-	rid				char(40)	PRIMARY KEY,
-	snaktype		text		not null,
-	property		varchar(20)		not null,
-	rank			smallint, -- Calculated from q_order
-	datatype		text,
-	valuetype		text,
-	value 			text, -- a representative value for faster access
-	did 			int 			not null,
-	FOREIGN KEY (property) REFERENCES Property (eid),
-	FOREIGN KEY (did) REFERENCES Datavalue (did)
+CREATE TABLE IF NOT EXISTS Reference ( -- Shall we reserve it?
+	rid 			char(40) 		PRIMARY KEY -- For hash!
 );
 
-CREATE TABLE IF NOT EXISTS Reference ( -- Shall we reserve it?
-	rid 			char(40), -- For hash!
-	cid				varchar(128),
-	FOREIGN KEY (cid) REFERENCES Claim (cid), 
-	FOREIGN KEY (rid) REFERENCES ReferneceItem (rid)
+CREATE TABLE IF NOT EXISTS ReferneceItem (
+	rid				char(40),
+	cid				char(40),
+	snaktype		varchar(9)		not null,
+	property		varchar(20)		not null,
+	rank			smallint, -- Calculated from q_order
+	datatype		varchar(20),
+	valuetype		varchar(20),
+	value 			varchar(32), -- a representative value for faster access
+	did 			int 			not null,
+	FOREIGN KEY (property) REFERENCES Property (eid),
+	FOREIGN KEY (cid) REFERENCES Claim (cid),
+	FOREIGN KEY (rid) REFERENCES Reference (rid)
 );
 
--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,5 @@
 import json
-import cPickle
+import cPickle, os, sys
 import MySQLdb
 
 def init_mysql():
@@ -7,61 +7,249 @@ def init_mysql():
     db = MySQLdb.connect(host="localhost", user="htl11", passwd="1234", db="Wikidata",\
         charset="utf8")
     cur = db.cursor()
-    cur.execute("DELETE FROM entity")
     cur.execute("SET foreign_key_checks = 0")
-    db.commit()
+    cur.execute("DELETE FROM entity")
+    cur.execute("DELETE FROM datavalue")
+    cur.execute("DELETE FROM time")
+    cur.execute("DELETE FROM wikientityid")
+    cur.execute("DELETE FROM quantity")
+    cur.execute("DELETE FROM claim")
+    # db.commit()
+    # cur.execute('''INSERT IGNORE INTO claim VALUES ("Q1$D6ECBA3F-C018-4C0C-A7FA-62B858782609", "statement", "value", "P1051", "1", "external-id", "string", "517", "41");''')
+    # cur.execute('''INSERT IGNORE INTO claim VALUES ("Q1$D6ECBA3F-C018-4C0C-A7FA-62B858782609", "statement", "value", "P1051", "1", "external-id", "string", "517", "41");''')
+    # cur.execute("INSERT INTO Datavalue VALUES (NULL, %s), (NULL, %s)", [("12"), ("12")])
+    # sql = "INSERT INTO Datavalue VALUES {0}".format("%s")
+    # cur.execute(sql, [("NULL", "12")])
     # cur.execute("SELECT * FROM entity")
     # print cur.fetchall()
 
 init_mysql()
+err_file = "err.log"
+err_f = open(err_file, "w")
+
+
+def insert_many(table, data_list, execute=True):
+    if data_list == []:
+        return
+    tpl = "%s," * len(data_list)
+    tpl = tpl[:-1]
+    sql = "INSERT IGNORE INTO %s VALUES " % table
+    sql += "{0}".format(tpl)
+    try:
+        if execute:
+            return cur.execute(sql, data_list)
+        else:
+            return sql % tuple(data_list)
+    except:
+        print table
+        print data_list
+        err_f.write(table + "\n")
+        err_f.write("\n".join(data_list) + "\n\n")
+
+def get_datavalue(valuetype, value):
+    shortvalue = ""
+    completevalue = []
+
+    if isinstance(value, dict):
+        for key, val in value.items():
+            if not isinstance(val, unicode):
+                value[key] = str(val).replace('"', '\\"').replace("'", "\\'")
+
+    if valuetype == "string":
+        shortvalue = value
+        if isinstance(value, dict):
+            shortvalue = value["value"]
+        completevalue = (shortvalue, )
+    elif valuetype == "wikibase-entityid":
+        if value["entity-type"] == "item":
+            shortvalue = "Q" + value["numeric-id"]
+        elif value["entity-type"] == "property":
+            shortvalue = "P" + value["numeric-id"]
+        else:
+            print value["entity-type"]
+            raise ValueError("entity-type not eq to property or item!")
+        completevalue = (shortvalue, )
+        valuetype = "wikientityid"
+    elif valuetype == "globecoordinate":
+        shortvalue = "(%s, %s)" % (value["latitude"], value["longitude"])
+        completevalue = tuple([value["latitude"], value["longitude"], \
+            value["altitude"], value["precision"], value["globe"]])
+    elif valuetype == "quantity":
+        shortvalue = value["amount"]
+        completevalue = tuple([value["amount"], value["upperBound"], \
+            value["lowerBound"], value["unit"]])
+    elif valuetype == "time":
+        shortvalue = value["time"]
+        completevalue = tuple([value["time"], value["timezone"], \
+            value["before"], value["after"], value["precision"], \
+            value["calendarmodel"]])
+    elif valuetype == "monolingualtext":
+        shortvalue = value["text"]
+        completevalue = tuple([value["language"], value["text"]])
+    else:
+        print valuetype, value
+        assert isinstance(value, str)
+        shortvalue = value
+        completevalue = (value, )
+        valuetype = ""
+        # raise ValueError("valuetype unknown!")
+
+    return shortvalue, completevalue, valuetype
+
+def commit_all(data_dict):
+    for table, data_list in data_dict:
+        insert_many(table, data_list, execute=True)
+
+
+table_list = ["alias", "badge", "claim", "datavalue", "description", "entity", \
+    "globecoordinate", "item", "label", "monolingaltext", "property", "qualifier", \
+    "quantity", "reference", "referenceitem", "sitelink", "string", "time", "wikientityid"]
+data_dict = {}
+for table in table_list:
+    data_dict[table] = []
 
 def load_data(fname):
     f = open(fname, "r")
+    
+    line_cnt = 0
+
+    cur.execute("SELECT did FROM Datavalue ORDER BY did DESC LIMIT 1")
+    did = cur.fetchone()
+    did = str(int(did[0]) if did is not None else -1)
+
     for line in f.xreadlines():
+        line_cnt += 1
+        print line_cnt
+        sys.stdout.flush()
+        if line_cnt % 1000 == 0:
+            commit_all(data_dict)
+            for table in table_list:
+                data_dict[table] = []
         line = line.strip()
         if line == "]" or line == "[":
             continue
         entity = json.loads(line[:-1])
-        eid, pageid, ns, title, lastrevid, modified, _type = entity["id"], int(0), \
-            int(0), "", int(0), "2015-02-27T14:37:20Z", entity["type"],
-        # assert eid == title
-        # cur.execute('''INSERT INTO entity VALUES (%s, %s, %s, %s, %s, %s)''', \
-        #     (eid, pageid, ns, lastrevid, modified, _type))
-        # db.commit()
+        eid, pageid, ns, title, lastrevid, modified, _type = entity["id"], str(0), \
+            str(0), "", str(0), "2015-02-27T14:37:20Z", entity["type"],
+        entity_data = (eid, pageid, ns, lastrevid, modified, _type)
+        data_dict["entity"].append(entity_data)
+        if _type == "item":
+            data_dict["item"].append((eid, ))
+        else:
+            data_dict["property"].append((eid, entity["datatype"]))
         try:
             alias_list = []
             for aliases in entity["aliases"].values():
                 for alias in aliases:
                     alias_list.append((eid, alias["language"], alias["value"]))
-            alias_list = alias_list[:1]
-            alias_tpl = ",".join(['%s'] * len(alias_list))
-            sql = "INSERT INTO alias VALUES {0}".format(alias_tpl)
-            cur.execute(sql, alias_list)        
+            data_dict["alias"] += alias_list
+      
 
             label_list = []
             for label in entity["labels"].values():
                 label_list.append((eid, label["language"], label["value"]))
-            label_list = label_list[:40]
-            label_tpl = ",".join(['%s'] * len(label_list))
-            sql = "INSERT INTO label VALUES {0}".format(label_tpl)
-            cur.execute(sql, label_list)  
+            data_dict["label"] += label_list
 
             desc_list = []
-            for desc in entity["description"].values():
+            for desc in entity["descriptions"].values():
                 desc_list.append((eid, desc["language"], desc["value"]))
+            data_dict["description"] += desc_list
 
-            desc_tpl = ",".join(['%s'] * len(desc_list))
-            sql = "INSERT INTO description VALUES {0}".format(desc_tpl)
-            cur.execute(sql, desc_list)  
+ 
+            # print "lastdid", did
+            datavalue_list = []
+            claim_list = []
+            qualifier_list = []
+            ref_list = []
+            ref_item_list = []
+            for claims in entity["claims"].values():
+                for claim in claims:
+                    did = str(int(did) + 1)
+                    cid, _type, rank = claim["id"], claim["type"], claim["rank"]
+                    rank = str(2 if rank == "deprecated" else 1 if rank == "normal" else 2)
+                    mainsnak = claim["mainsnak"]
+                    snaktype, pid, datatype = mainsnak["snaktype"], mainsnak["property"], \
+                        mainsnak["datatype"]
+                    value = ""
+                    if "datavalue" in mainsnak:
+                        valuetype = mainsnak["datavalue"]["type"]
+                        value = mainsnak["datavalue"]["value"]
+                        shortvalue, completevalue, valuetable = get_datavalue(valuetype, value)
+                        completevalue = (did, ) + completevalue
+                        data_dict[valuetable].append(completevalue)
+                        datavalue_list.append((did, valuetype)) 
+
+                    claim_list.append((cid, _type, snaktype, pid, rank, datatype, \
+                        valuetype, shortvalue, did))
+
+                    if "qualifiers" in claim:                
+                        qualifier_order = {}
+                        # print claim
+                        for i, pid in enumerate(claim["qualifiers-order"]):
+                            qualifier_order[pid] = str(i)
+                        for qualifiers in claim["qualifiers"].values():
+                            for qua in qualifiers:
+                                did = str(int(did) + 1)
+                                # print qua
+                                qid, qsnaktype, qpid, qdatatype = qua["hash"], \
+                                    qua["snaktype"], qua["property"], qua["datatype"]
+                                qrank = qualifier_order[qpid]
+                                qvaluetype = ""
+                                qvalue = ""
+                                if "datavalue" in qua:
+                                    qvalue = qua["datavalue"]
+                                    qvaluetype = qvalue["type"]
+                                    qvalue = qvalue["value"]
+                                    qshortvalue, qcompletevalue, qvaluetable = get_datavalue(qvaluetype, qvalue)
+                                    qcompletevalue = (did,) + qcompletevalue
+                                    data_dict[qvaluetable].append(qcompletevalue)
+                                    datavalue_list.append((did, qvaluetype))
+                                qualifier_list.append((qid, cid, qsnaktype, qpid, qrank, qdatatype,\
+                                    qvaluetype, qshortvalue, did))
+
+                    if "references" in claim:
+                        for ref in claim["references"]:
+                            ref_order = {}
+                            for i, pid in enumerate(ref["snaks-order"]):
+                                ref_order[pid] = str(i)
+                            rid = ref["hash"]
+                            # print rid, eid
+                            ref_list.append((rid, cid))
+                            for snaks in ref["snaks"].values():
+                                for snak in snaks:
+                                    did = str(int(did) + 1)
+                                    rsnaktype, rpid, rdatatype = snak["snaktype"], snak["property"],\
+                                        snak["datatype"]
+                                    rrank = ref_order[rpid]
+                                    rvalue = ""
+                                    rvaluetype = ""
+                                    if "datavalue" in snak:
+                                        rvalue = snak["datavalue"]
+                                        rvaluetype = rvalue["type"]
+                                        rvalue = rvalue["value"]
+                                        rshortvalue, rcompletevalue, rvaluetable = get_datavalue(rvaluetype, rvalue)
+                                        rcompletevalue = (did,) + rcompletevalue
+                                        data_dict[rvaluetable].append(rcompletevalue)
+                                        datavalue_list.append((did, rvaluetype))
+                                    ref_item_list.append((rid, rsnaktype, rpid, rrank, rdatatype, \
+                                        rvaluetype, rshortvalue, did))
+
+            data_dict["datavalue"] += datavalue_list
+            data_dict["claim"] += claim_list
+            data_dict["qualifier"] += qualifier_list
+            data_dict["reference"] += ref_list
+            data_dict["referneceitem"] += ref_item_list
+
         except:
-            for i in label_list:
-                print len(i[1]), len(i[2])
-            print sql
+            # for i in label_list:
+            #     print len(i[1]), len(i[2])
+            # print sql
             raise
 
 
-        db.commit()
-        break
+        # db.commit()
+        # break
+    print line_cnt
     f.close()
     pass
 
